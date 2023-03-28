115467b0d644604211d4cef8b3019ee777f0477c
Awais Rao <108420364+Awais-Rao@users.noreply.github.com>
2023-03-28 11:28:40 +0500

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,14 +18,9 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener('scroll', function () {
 
         if (window.scrollY > 200) {
-            document.getElementById('navbar_top').classList.add('fixed-top', 'shadow');
-            // add padding top to show content behind navbar
-            navbar_height = document.querySelector('.navbar').offsetHeight;
-            document.body.style.paddingTop = navbar_height + 'px';
+            document.getElementById('navbar_top').classList.add('shadow', 'opacity-100');
         } else {
-            document.getElementById('navbar_top').classList.remove('fixed-top', 'shadow');
-            // remove padding top from body
-            document.body.style.paddingTop = '0';
+            document.getElementById('navbar_top').classList.remove('shadow', 'opacity-100');
         }
     });
 });
